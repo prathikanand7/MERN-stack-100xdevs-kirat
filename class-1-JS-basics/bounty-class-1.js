dfe1677eebfb4d77ad5bd33330fe0c7213b1c83d
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2024-02-15 23:39:36 +0530

    add callback functions

---
+++ class-1-JS-basics/bounty-class-1.js
@@ -0,0 +1,8 @@
+let sum = 0;
+
+for (let i = 0; i <= 100000000000000000; i++){
+    sum += i;
+}
+console.log(sum);
+
+//answer was htop command in his macOS is wrong..and not able to detect the RAM usage correctly
---
+++ class-1-JS-basics/functions.js
@@ -0,0 +1,54 @@
+function sum(num1, num2, fnToCall) {
+    //take 2 numbers do the sum and return
+    
+    let result = num1 + num2
+    fnToCall(result);
+}
+
+// const valueOfSum = sum(3, 4);
+// console.log(valueOfSum);
+
+// callbacks - when a function can take another function as input
+
+function displayResult(data) {
+    console.log("Result of the sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+// You are only allowed to call one function after this
+// How will you displayResult of a sum
+
+// Ans: we sent a function as an argument inside another function
+const ans = sum(1, 1, displayResult);
+
+function calculateArithmetic(a, b, type) {
+    if (type == "sum") { return sumofnumbers(a, b) }
+    if (type == "minus") { return sub(a, b); }
+}
+
+function sumofnumbers(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+
+const answer = calculateArithmetic(10, 5, "sum")
+console.log(answer);
+
+
+
+// calling a function inside another function by passing a function as an argument - callback. means calling back another function from inside a function.
+function calculateArithmetic_second(a, b, finalcalculateArithmetic) {
+    const ans = finalcalculateArithmetic(a, b);
+    return ans;
+}
+
+
+const answer_again = calculateArithmetic_second(10, 5, sumofnumbers)
+console.log(answer_again);
