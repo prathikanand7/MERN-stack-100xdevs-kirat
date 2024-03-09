80831aca42f83ca3de0b9f8aa6f3de2b89fda748
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2024-03-09 22:00:08 +0530

    learning express.js

--- week-02-JS_http_express_server/index.js
+++ week-02-JS_http_express_server/index.js
@@ -3,15 +3,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/route-handler", function (req, res) {
+    //run Machine Learning model
+    res.json({
+        name: "Hello World again!",
+        age: 25
+    })
+})
+
 app.get('/', function (req, res) {
     res.send("Hello World!")
 })
 
-
-app.post("/backend-api/conversation", function (req, res) {
-    //run Machine Learning model
-    res.send("Hello World again!")
-})
 
 app.listen(port, function () {
     console.log('Example app is listening to port ${port}')
