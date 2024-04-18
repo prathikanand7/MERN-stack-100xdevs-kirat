3bbadb94432898da3a4a4578a53559043a3d2014
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2024-04-18 17:31:02 +0530

    using React.lazy and suspense component in the react app

--- week-7.1-learning-react-routes/src/App.jsx
+++ week-7.1-learning-react-routes/src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter,Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
-import { Dashboard } from './components/Dashboard';
-import { Landing } from './components/Landing';
+//import { Dashboard } from './components/Dashboard';
+//import { Landing } from './components/Landing';
+import React, { Suspense } from 'react';
+const Dashboard = React.lazy(() => import('./components/Dashboard'))
+const Landing = React.lazy(() => import('./components/Landing'))
 
 //client site routing
 function App() {
  
+  //suspense API to show "Loading..." to wait for the callback to return and render it on the screen
   return (
     <div>
       <BrowserRouter>
         <Appbar />
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Suspense fallback={"Loading..."}><Dashboard /></Suspense>} />
+          <Route path="/" element={<Suspense fallback={"Loading..."}><Landing /></Suspense>} />
         </Routes>
       </BrowserRouter>
     </div>
