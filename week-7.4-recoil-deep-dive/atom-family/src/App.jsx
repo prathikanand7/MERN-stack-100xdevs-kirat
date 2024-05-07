17d3395100483d57101e0ed3f29301cf752c1814
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2024-05-08 00:39:52 +0530

    7.4-atom family deep dive

--- week-7.4-recoil-deep-dive/atom-family/src/App.jsx
+++ week-7.4-recoil-deep-dive/atom-family/src/App.jsx
@@ -1,13 +1,37 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
 import { todosAtomFamily } from './atoms';
+import { useEffect } from 'react';
 
 function App() {
+  
   return <RecoilRoot>
+    <UpdaterComponent/>
     <Todo id={1}/>
     <Todo id={2} />
-  </RecoilRoot>
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+  </RecoilRoot> 
+}
+
+function UpdaterComponent() {
+  const updateTodo = useSetRecoilState(todosAtomFamily(2));
+
+  useEffect(() => {
+    setTimeout(() => {
+      updateTodo({
+        id: "2",
+        title: "new todo",
+        description: "new todo"
+      })
+    }, 5000)
+  }, [])
+  return <div></div>
 }
 
 function Todo({id}) {
